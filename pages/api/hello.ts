33693026d24432941a571664c2101b95524f338e
fromabby <60252891+fromabby@users.noreply.github.com>
2022-05-31 12:50:14 +0800

    testing prisma and nextjs

---
+++ pages/api/hello.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const { id } = await prisma.user.create({
+        data: {
+            nearId: 'abby',
+            discord: { create: [{ username: 'Myoui', tag: '1234' }] },
+        },
+    })
+
+    const result = await prisma.user.findUnique({
+        where: {
+            id,
+        },
+        include: {
+            discord: true,
+        },
+    })
+
+    res.status(200).json(result)
+}
